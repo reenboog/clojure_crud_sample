171cfac32edbd02ebbf26f9300e8ce0374c6d125
reenboog <alex.gievsky@gmail.com>
2015-10-25 16:25:51 +0600

    jdbc fix.

--- src/tttback/db.clj
+++ src/tttback/db.clj
@@ -5,5 +5,5 @@
 (defdb db (postgres {:db (get env :tttback-db "restful_dev")
                      :user (get env :tttback-db-user "restful_dev")
                      :password (get env :tttback-db-pass "pass_dev")
-                     :host (get env :tttback-db-host "localhost")
+                     :host (get env :tttback-db-host "ec2-54-235-78-155.compute-1.amazonaws.com")
                      :port (get env :tttback-db-port 5432)}))
