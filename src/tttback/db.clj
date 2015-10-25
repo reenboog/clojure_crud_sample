87138d370f9ed5968394da99505a869748195599
reenboog <alex.gievsky@gmail.com>
2015-10-25 17:39:07 +0600

    users fix.

--- src/tttback/db.clj
+++ src/tttback/db.clj
@@ -4,6 +4,7 @@
 
 (defdb db (postgres {:db (get env :tttback-db "restful_dev")
                      :user (get env :tttback-db-user "restful_dev")
-                     :password (get env :tttback-db-pass "pass_dev")
-                     :host (get env :tttback-db-host "ec2-54-235-78-155.compute-1.amazonaws.com")
-                     :port (get env :tttback-db-port 5432)}))
+                     :password (get env :tttback-db-pass "pass_dev")}))
+
+;:host (get env :tttback-db-host "ec2-54-235-78-155.compute-1.amazonaws.com")
+;:port (get env :tttback-db-port 5432)
