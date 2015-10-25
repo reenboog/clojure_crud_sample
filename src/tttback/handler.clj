cc5c3c032ad8eafc406ad00d4dabba35ba3792b2
reenboog <alex.gievsky@gmail.com>
2015-10-25 13:22:38 +0600

    jdbc fix.

--- src/tttback/handler.clj
+++ src/tttback/handler.clj
@@ -72,7 +72,7 @@
         {:status 201
          :headers {"Location" (str "/products/" (:id new-prod))}})))
 
-  (route/not-found (response {:message ~(System/getenv "DATABASE_URL")})))
+  (route/not-found (response {:message (System/getenv "DATABASE_URL")})))
 
 (defn wrap-log-request [handler]
   (fn [req]
